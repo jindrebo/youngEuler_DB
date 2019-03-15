b3acb02af705c7659ab62b2db5d47156b2e7a943
Cato Støle Robstad <31210116+h183570@users.noreply.github.com>
2019-03-15 13:15:23 +0100

    Update data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,12 @@
-insert into sporsmal (id, niva, spm, svar) values(1, 1, 'Vis 2+2', 'fire');
+insert into 
+sporsmal (id, niva, spm, svar) 
+values	
+(1, 1, 'Vis 2+2', 'fire'),
+(2, 1, 'Vis 2+3', 'fem'),
+(3, 1, 'Vis 2+2+3', 'syv'),
+(4, 1, 'Vis 3+3', 'seks'),
+(5, 2, 'Vis 2*2', 'fire'),
+(6, 2, 'Vis 2*3', 'seks');
+
+
 
