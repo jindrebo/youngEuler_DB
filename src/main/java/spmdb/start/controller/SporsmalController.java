b5c181b7a5642fb04fc4668a2842b04e90ad0822
571733 <jindrebo@hotmail.no>
2019-03-21 09:42:34 +0100

    try fix, not work

--- src/main/java/spmdb/start/controller/SporsmalController.java
+++ src/main/java/spmdb/start/controller/SporsmalController.java
@@ -4,23 +4,34 @@ import java.util.List;
 
 import javax.annotation.Resource;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import spmdb.start.dao.spmRepository;
 import spmdb.start.entity.Sporsmal;
-import spmdb.start.service.SporsmalService;
+
 
 @RestController
 @RequestMapping("/sporsmal")
 public class SporsmalController {
 	
-	@Resource
-	SporsmalService sporsmalService;
 	
+	spmRepository sporsmalService;
+	
+	
+	@Autowired
+	public SporsmalController(spmRepository sporsmalService) {
+		super();
+		this.sporsmalService = sporsmalService;
+	}
+
+
+
 	@GetMapping (value = "/sporsmalListe")
-	public List<Sporsmal> getSporsmal() {
-		return sporsmalService.finnAlle();
+	public Iterable<Sporsmal> getSporsmal() {
+		return sporsmalService.findAll();
 	}
 
 }
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,5 @@
-insert into sporsmal (id, niva, spm, svar) values(1, 1, 'Vis 2+2', 'fire');
+insert into sporsmal (id, niva, spm, svar) 
+values
+(default, 1, 'Vis 2+2', 'fire'),
+(default, 1, 'Vis 2+3', 'fem');
 
